cf9ab4bde2e68e6e15752e98b8ecac3c4a8d64de
Ilya Kozhevnikov <git@kozhevnikov.com>
2018-01-24 16:10:49 +0000

    Lint HTML

--- test/document.js
+++ test/document.js
@@ -1,9 +1,18 @@
 const { describe, it } = require('mocha');
+const lint = require('html5-lint');
 require('should');
 
 const exec = require('./exec');
 
 describe('Document', () => {
+  it('should be valid', (done) => {
+    lint(exec('features/'), (error, { messages }) => {
+      if (error) throw error;
+      messages.should.be.empty();
+      done();
+    });
+  });
+
   it('should log doctype', () => {
     exec('features/feature.feature').should.startWith('<!DOCTYPE html>');
   });
